1563c7a67ae69829b0b73362bc91bad95a1a7020
Dana <danalkhashram@gmail.com>
2025-08-12 11:42:21 +0300

    fix Dana/Menu-Details

--- src/components/MenuDetails/MenuDetails.jsx
+++ src/components/MenuDetails/MenuDetails.jsx
@@ -1,88 +1,90 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import * as menuService from "../../services/menuService";
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
 
 const MenuDetails = ({ handleDeleteMenu, user }) => {
-    const { restaurantId } = useParams();
-    const [menus, setMenus] = useState([]);
+  const { restaurantId } = useParams();
+  const [menus, setMenus] = useState([]);
 
-    useEffect(() => {
-        const data = menuService.indexByRestaurant(restaurantId)
-        setMenus(data)
+  useEffect(() => {
+    const fetchMenus = async () => {
+      try {
+        const data = await menuService.indexByRestaurant(restaurantId);
+        setMenus(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchMenus();
+  }, [restaurantId]);
 
-    }, [restaurantId]);
+  const mainCourse = menus.filter(menu => menu.type === "main");
+  const drinks = menus.filter(menu => menu.type === "drinks");
+  const dessert = menus.filter(menu => menu.type === "dessert");
 
-    const mainCourse = menus.filter(menu => menu.type === "main");
-    const drinks = menus.filter(menu => menu.type === "drinks");
-    const dessert = menus.filter(menu => menu.type === "dessert");
+  return (
+    <main className="menu-details">
+      <Link to={`/restaurant/${restaurantId}/menu/new`}>Add</Link>
 
-    return (
-        <main className="menu-details">
-            {mainCourse.length > 0 && (
-                <>
-                    <h2>Main Course</h2>
-                    <ul>
-                        {mainCourse.map(menu => (
-                            <li key={menu._id}>
-                                {menu.title} 
-                                {menu.description} - {menu.price} BD
-                                {menu.author?._id === user?._id && (
-                                    <div>
-                                        <button onClick={() => handleDeleteMenu(menu._id)}>Delete</button>
-                                        <Link to={`/restaurant/${restaurantId}/menu/${menu._id}}/edit`}>Edit</Link>
-                                    </div>
+      {mainCourse.length > 0 && (
+        <>
+          <h2>Main Course</h2>
+          <ul>
+            {mainCourse.map(menu => (
+              <li key={menu._id}>
+                <strong>{menu.title}</strong> - {menu.description} - {menu.price} BD
+                {menu.author?._id === user?._id && (
+                  <div>
+                    <button onClick={() => handleDeleteMenu(menu._id)}>Delete</button>
+                    <Link to={`/restaurant/${restaurantId}/menu/${menu._id}/edit`}>Edit</Link>
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
 
-                                )}
-                            </li>
-                        ))}
-                    </ul>
-                </>
-            )}
+      {dessert.length > 0 && (
+        <>
+          <h2>Dessert</h2>
+          <ul>
+            {dessert.map(menu => (
+              <li key={menu._id}>
+                <strong>{menu.title}</strong> - {menu.description} - {menu.price} BD
+                {menu.author?._id === user?._id && (
+                  <div>
+                    <button onClick={() => handleDeleteMenu(menu._id)}>Delete</button>
+                    <Link to={`/restaurant/${restaurantId}/menu/${menu._id}/edit`}>Edit</Link>
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
 
-            {dessert.length > 0 && (
-                <>
-                    <h2>Dessert</h2>
-                    <ul>
-                        {dessert.map(menu => (
-                            <li key={menu._id}>
-                                {menu.title}
-                                {menu.description} - {menu.price} BD
-                                {menu.author?._id === user?._id && (
-                                    <div>
-                                        <button onClick={() => handleDeleteMenu(menu._id)}>Delete</button>
-                                        <Link to={`/restaurant/${restaurantId}/menu/${menu._id}}/edit`}>Edit</Link>
-                                    </div>
-                                )}
-                            </li>
-                        ))}
-                    </ul>
-                </>
-            )}
-
-            {drinks.length > 0 && (
-                <>
-                    <h2>Drinks</h2>
-                    <ul>
-                        {drinks.map(menu => (
-                            <li key={menu._id}>
-                                {menu.title} 
-                                {menu.description} - {menu.price} BD
-                                {menu.author?._id === user?._id && (
-                                    <div>
-                                        <button onClick={() => handleDeleteMenu(menu._id)}>Delete</button>
-                                        <Link to={`/restaurant/${restaurantId}/menu/${menu._id}}/edit`}>Edit</Link>
-                                    </div>
-                                )}
-                            </li>
-                        ))}
-                    </ul>
-                </>
-            )}
-
-        </main>
-    );
+      {drinks.length > 0 && (
+        <>
+          <h2>Drinks</h2>
+          <ul>
+            {drinks.map(menu => (
+              <li key={menu._id}>
+                <strong>{menu.title}</strong> - {menu.description} - {menu.price} BD
+                {menu.author?._id === user?._id && (
+                  <div>
+                    <button onClick={() => handleDeleteMenu(menu._id)}>Delete</button>
+                    <Link to={`/restaurant/${restaurantId}/menu/${menu._id}/edit`}>Edit</Link>
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </main>
+  );
 };
 
 export default MenuDetails;
